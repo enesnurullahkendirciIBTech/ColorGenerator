488848076c65b578c7e6f1c02e5e42238a84996d
Volkan Sönmez <sonmez.volkan92@gmail.com>
2024-05-29 13:46:27 +0300

    Colors are generated

--- src/model/ColorModel.py
+++ src/model/ColorModel.py
@@ -13,18 +13,20 @@ class ColorModel:
 
     def createAssets(self, colorName, colorPath):
         self.__createRootFolder()
-        self.__createPath(self.__getRootPath(), colorName, colorPath)
+        self.__createPath(self.__getRootPath(), colorName, colorPath, "")
 
     def __createRootFolder(self):
         if not os.path.exists(self.__getRootPath()):
             os.makedirs(self.__getRootPath())
             self.__addInfoJson(self.__getRootPath())
 
-    def __createPath(self, filePath, colorName, colorPath):
+    def __createPath(self, filePath, colorName, colorPath, assetName):
         if colorPath.nextPath is None:
-            filePath = filePath + "/" + self.theme['prefix'] + colorPath.name + ".colorset"
+            assetName = self.theme['prefix'] + assetName + colorPath.name + ".colorset"
+            filePath = filePath + "/" + assetName + colorPath.name + ".colorset"
         else:
             filePath = filePath + "/" + colorPath.name
+            assetName += colorPath.name + "_"
 
         if not os.path.exists(filePath):
             os.makedirs(filePath)
@@ -32,9 +34,10 @@ class ColorModel:
                 self.__addInfoJson(filePath)
 
         if colorPath.nextPath is not None:
-            self.__createPath(filePath, colorName, colorPath.nextPath)
+            self.__createPath(filePath, colorName, colorPath.nextPath, assetName)
         else:
             ColorAssetsModel().create(filePath, colorName, self.isDark)
+            self.__generateColorSwiftFile(assetName)
 
     def __getRootPath(self):
         return self.path + "/" + self.theme['name'] + ".xcassets"
@@ -51,3 +54,19 @@ class ColorModel:
             with open(filePath, 'w') as contents:
                 json.dump(info, contents)
 
+
+    def __generateColorSwiftFile(self, assetName):
+        if self.isDark:
+            colorName = assetName
+            if colorName.startswith("daf_"):
+                colorName = colorName[len("daf_"):]
+                if colorName.endswith(".colorset"):
+                    colorName = colorName[:-len(".colorset")]
+
+            if assetName.endswith(".colorset"):
+                assetName = assetName[:-len(".colorset")]
+            colorGenerated = f"public var {colorName}: UIColor {{   \nR.color.{assetName}()~ \n}}"
+
+            filePath = self.__getRootPath() + "/" + self.theme['name']
+            with open(filePath, "a") as file:
+                file.write("\n" + colorGenerated)
